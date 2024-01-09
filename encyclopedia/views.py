f06c43010399d500e4a0d9ea85bb9bab5f0fd39c
adcerro <adcerro@uninorte.edu.co>
2024-01-08 22:58:18 -0500

    create page implemented

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,15 +5,26 @@ from random import choice
 
 from . import util
 
+    
 class CreateForm(forms.Form):
     common={'class': 'form-control', 'style': 'margin-bottom:5px;margin-top:5px'}
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Page Title'}|common))
-    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Page Content'}|common))
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Page Content (in markdown)'}|common))
 
 def create(request):
-    return render(request, "encyclopedia/create.html",{
-        "form": CreateForm()
-    })
+    if request.method == "POST":
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            if title.lower() in [t.lower() for t in util.list_entries()]:
+                return render(request, "encyclopedia/create.html",{"form": form,"invalid":True})
+            else:
+                util.save_entry(title=title,content=form.cleaned_data["content"])
+                return HttpResponseRedirect(f"wiki/{title}")
+        else:
+            return render(request, "encyclopedia/create.html",{"form": form})
+    else:
+        return render(request, "encyclopedia/create.html",{"form": CreateForm()})
 
 def index(request):
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries(),})
@@ -34,8 +45,7 @@ def search(request,query):
     query = request.GET.get('q')
     if query in util.list_entries():
         return HttpResponseRedirect(f"wiki/{query}")
-    elif query != None:
-        return render(request,"encyclopedia/search.html",{
+    return render(request,"encyclopedia/search.html",{
             "query": query,
             "entries": [entry for entry in util.list_entries() if query.lower() in entry.lower()],})
 
